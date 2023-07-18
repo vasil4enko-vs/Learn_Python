e10f5dc609ecd324d54e34e50f369f2cdbe7814c
vasil4enko-vs <vvs170@gmail.com>
2023-07-18 14:36:28 +0300

    Ввод целочисленного значения.

---
+++ learn-python-selfedu/listing/02-moi-pervyye-shagi-v-python/02.04-funktsii-print-i-input/02.04.06.py
@@ -0,0 +1,21 @@
+"""2.4. Функции print и input.
+
+Примеры использования функции input().
+Ввод целочисленного значения. Функция `int()`.
+"""
+
+# a = input()
+# print(a, type(a))
+# b = abs(a)
+# print(b)
+
+# a = input()
+# a = int(a)
+# print(a, type(a))
+# b = abs(a)
+# print(b)
+
+a = int(input())
+print(a, type(a))
+b = abs(a)
+print(b)
