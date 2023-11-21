18f571cc777cf7f472a151d5ff34063692816375
vasil4enko-vs <vvs170@gmail.com>
2023-11-21 14:36:13 +0200

    05.02. Листинг

---
+++ learn-python-selfedu/listing/05-tsikly-iteratory-i-generatory spiskov/05.02/05.02.02.py
@@ -0,0 +1,14 @@
+"""5.2. Операторы break, continue и else.
+
+Пример использования оператора break.
+"""
+
+print("Запуск цикла")
+
+i = 0
+while True:
+    i += 1
+    print(i)
+    break
+
+print("Завершение цикла")
